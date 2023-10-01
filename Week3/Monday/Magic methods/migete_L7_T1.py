7a96a2a2ffa3ead5c0983ccfeb3f5f715c685eb4
Milen Temelkov <98092794+Helmieto@users.noreply.github.com>
2023-10-01 13:51:57 +0300

    Update migete_L7_T1.py
    Fixed the tasks, as I have solved it completly wrong

--- Week3/Monday/Magic methods/migete_L7_T1.py
+++ Week3/Monday/Magic methods/migete_L7_T1.py
@@ -2,9 +2,19 @@ class Fibs():
     def __init__(self):
         self.iterator = 0
         self.iterator_next = 1
+
     def __next__(self):
-        curr = self.iterator
-        self.iterator, self.iterator_next = self.iterator_next, self.iterator_next + 1
-        return curr
+        temp = self.iterator
+        self.iterator = self.iterator_next
+        self.iterator_next += temp
+        return temp
     def __iter__(self):
         return self
+
+
+fib = Fibs()
+
+for i in fib:
+    if i > 100:
+        break
+    print(i)
