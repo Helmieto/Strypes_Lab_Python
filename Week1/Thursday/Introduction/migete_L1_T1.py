27702e514289666e35aa1c5440b410fb2c1af5bf
Milen Temelkov <98092794+Helmieto@users.noreply.github.com>
2023-10-01 13:43:22 +0300

    Update task 1
    Added parenteces, because the solution was wrong without them.

--- Week1/Thursday/Introduction/migete_L1_T1.py
+++ Week1/Thursday/Introduction/migete_L1_T1.py
@@ -1,7 +1,7 @@
 import math
 import sys
 
-a= float(sys.argv[1])
+a = float(sys.argv[1])
 b = float(sys.argv[2])
 c = float(sys.argv[3])
 
@@ -9,12 +9,12 @@ if a != 0:
    discriminant = b**2 - 4 * a * c
 
    if discriminant > 0:
-       x1 = (-b - math.sqrt(discriminant)) / 2 * a
-       x2 = (-b + math.sqrt(discriminant)) / 2 * a
+       x1 = (-b - math.sqrt(discriminant)) / (2 * a)
+       x2 = (-b + math.sqrt(discriminant)) / (2 * a)
        print(x1, "|", x2)
 
    elif discriminant == 0:
-       x = -b / 2 * a
+       x = -b / (2 * a)
        print(x)
 
    else:
